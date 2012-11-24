4b11b3d430d994c07096371598d5bbe874860ea6
Devon Kearns <dookie@kali.org>
2012-11-24 07:10:21 -0700

    Imported Upstream version 0.7.4.2

---
+++ autogen.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+autoreconf --install --force
+# when not preparing a tarball, consider instead:
+# autoreconf --symlink --install
---
+++ include/ec_version.h
@@ -0,0 +1,17 @@
+
+
+#ifndef EC_VERS_H
+#define EC_VERS_H
+
+#define EC_VERSION            "NG-0.7.4.2"
+#define EC_VERSION_MAJOR      0
+#define EC_VERSION_MINOR      7
+#define EC_VERSION_REVISION   4
+#define EC_PROGRAM            "ettercap"
+#define EC_COPYRIGHT          "2001-2005"
+#define EC_AUTHORS            "ALoR & NaGA"
+
+#endif
+
+/* EOF */
+
